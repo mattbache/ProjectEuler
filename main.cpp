892f74f72fc53e1e8f02652775acd85203709870
Matthew Bache <mattbache@gmail.com>
2022-01-20 16:54:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 //
 
 #include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+long Algo1(int i);
+long Algo2(int i);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -13,14 +18,29 @@ int main(int argc, const char * argv[]) {
     int i;
     std::cin >> i;
     std::cout << "\nCalculating the sum of all the multiples of 3 or 5 below " << i << "\n";
-    int ourAnswer = 0;
-    for (int j = 1; j < i; j++) {
-        if (j%3 == 0 || j%5 == 0) {
-            ourAnswer = ourAnswer + j;
-        }
-    }
     
+    auto start = high_resolution_clock::now();
+    long ourAnswer = Algo1(i);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
     std::cout << "Our Answer is " << ourAnswer << "\n";
+    std::cout << "Brute force Algo1 in " << duration.count() << " microseconds\n";
     
     return 0;
+}
+
+long Algo1(int target) {
+    long ourAnswer = 0;
+    for (int i = 1; i < target; i++) {
+        if (i%3 == 0 || i%5 == 0) {
+            ourAnswer = ourAnswer + i;
+        }
+    }
+    return ourAnswer;
+}
+
+long Algo2(int target) {
+    long ourAnswer = 0;
+
+    return ourAnswer;
 }
