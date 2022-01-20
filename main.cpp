97ef9cb06446c3be16c77872293e7dba1f29b625
Matthew Bache <mattbache@gmail.com>
2022-01-20 17:43:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,27 +9,35 @@
 #include <chrono>
 using namespace std::chrono;
 
-long Algo1(int i);
-long Algo2(int i);
+long Algo1(long i);
+long Algo2(long i);
+long sumDivisibleBy(long target, int i);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Input natural number to calculate\n";
-    int i;
+    long i;
     std::cin >> i;
     std::cout << "\nCalculating the sum of all the multiples of 3 or 5 below " << i << "\n";
     
     auto start = high_resolution_clock::now();
     long ourAnswer = Algo1(i);
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration1 = duration_cast<microseconds>(stop - start);
+    std::cout << "Brute force Algo1 in " << duration1.count() << " MICROseconds\n";
     std::cout << "Our Answer is " << ourAnswer << "\n";
-    std::cout << "Brute force Algo1 in " << duration.count() << " microseconds\n";
-    
+
+    start = high_resolution_clock::now();
+    ourAnswer = Algo2(i);
+    stop = high_resolution_clock::now();
+    auto duration2 = duration_cast<nanoseconds>(stop - start);
+    std::cout << "Smart Algo2 in " << duration2.count() << " NANOseconds\n";
+    std::cout << "Our Answer is " << ourAnswer << "\n";
+
     return 0;
 }
 
-long Algo1(int target) {
+long Algo1(long target) {
     long ourAnswer = 0;
     for (int i = 1; i < target; i++) {
         if (i%3 == 0 || i%5 == 0) {
@@ -39,8 +47,15 @@ long Algo1(int target) {
     return ourAnswer;
 }
 
-long Algo2(int target) {
+long Algo2(long target) {
     long ourAnswer = 0;
-
+    ourAnswer = sumDivisibleBy(target,3)+sumDivisibleBy(target,5)-sumDivisibleBy(target,15);
     return ourAnswer;
+}
+
+long sumDivisibleBy(long target, int i) {
+    long p = (target-1)/i;
+    long sum = 0;
+    sum = i*(p*(p+1))/2;
+    return sum;
 }
